ff91d6d26fb41594cd9669d453ae35213e4f3e1c
andreww15 <andresgf925@gmail.com>
2025-06-12 16:07:20 -0300

    importo TemplateComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { TemplateComponent } from './pages/template/template.component';
 
 export const routes: Routes = [
   {
