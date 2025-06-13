6ce2d0826185d9fe81a7b5e6c31aeaeb6b8dd862
andreww15 <andresgf925@gmail.com>
2025-06-13 15:49:09 -0300

    agrego botones update y delete

--- src/app/pages/zones/zones.ts
+++ src/app/pages/zones/zones.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-zones',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './zones.html',
   styleUrl: './zones.css'
 })
 export class Zones {
-
+zones = [
+  { id: 1, name: 'Zone A', location: 'New York', radius: '10 km', deliveries: 5 },
+  { id: 2, name: 'Zone B', location: 'Chicago', radius: '8 km', deliveries: 3 },
+  { id: 3, name: 'Zone C', location: 'Los Angeles', radius: '12 km', deliveries: 7 },
+];
 }
