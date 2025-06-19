613e561da2e7a3be303c18fc2ca55dccf83a2ed6
Valentín <correavale2004@gmail.com>
2025-06-19 16:07:21 -0300

    agrego login funcional y uso de tokens en requests a la api de zonas

--- src/app/services/zone.service.ts
+++ src/app/services/zone.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import axios from 'axios';
 import { config } from '../config/env';
+import { axiosService } from './axios-client';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +11,9 @@ export class ZoneService {
 
   async getZones(): Promise<Array<{ id: number, name: string, location: {lat: string, lng: string}, radius: number, deliveries: number[] }>> {
     
-    const token = localStorage.getItem('token');
+   // const token = localStorage.getItem('token');
 
-    const response = await axios.get(config.urls.getZones, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": 'application/json'
-      }
+    const response = await axiosService.get(config.urls.getZones, {
     });
 
     return response.data
