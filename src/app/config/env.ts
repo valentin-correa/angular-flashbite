314f7f84ddf48ce88482ccfc3daba48ef21cc76a
santiwg <santiwgwuri@gmail.com>
2025-07-14 21:23:22 -0300

    Cambio URL del backend

--- src/app/config/env.ts
+++ src/app/config/env.ts
@@ -1,7 +1,7 @@
 export const config = {
   urls: {
     getFood: 'https://www.themealdb.com/api/json/v1/1/random.php',
-    getZones: 'http://localhost:3002/zone',
+    getZones: 'http://localhost:3000/zone',
     auth:'http://localhost:3001'
   },
 };
